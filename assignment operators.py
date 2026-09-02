5810f7ac2ebc8196d83f3f43ca37da17211ff0f1
gujjalajyothika7-ai <gujjalajyothika7@gmail.com>
2026-09-02 12:45:41 +0530

    Add files via upload

---
+++ assignment operators.py
@@ -0,0 +1,22 @@
+#assignment operators
+x=10
+x+=5
+print(x)
+x-=2
+print(x)
+x*=3
+print(x)
+x/=2
+print(x)
+x//=4
+print(x)
+x%=2
+print(x)
+#bank balance
+balance=1000
+deposit=500
+balance+=deposit
+print("AFTER DEPOSIT:",balance)
+withdraw=200
+balance-=withdraw
+print("AFTER WITHDRAWAL:",balance)
